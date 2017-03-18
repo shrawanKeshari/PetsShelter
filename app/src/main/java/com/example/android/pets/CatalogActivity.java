b010bd3ab72fed9675632cee4928c75ed0d144d7
sonu <sonu190893@gmail.com>
2017-03-19 00:20:00 +0530

    Display name, breed, gender and weight in listView

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -130,7 +130,9 @@ public class CatalogActivity extends AppCompatActivity
         String[] projection = {
                 PetEntry._ID,
                 PetEntry.COLUMN_PET_NAME,
-                PetEntry.COLUMN_PET_BREED};
+                PetEntry.COLUMN_PET_BREED,
+                PetEntry.COLUMN_PET_GENDER,
+                PetEntry.COLUMN_PET_WEIGHT};
 
 
         return new CursorLoader(this,
--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -33,15 +33,33 @@ public class PetCursorAdapter extends CursorAdapter {
 
         int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
         int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+        int genderColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_GENDER);
+        int weightColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_WEIGHT);
 
         String petName = cursor.getString(nameColumnIndex);
         String petBreed = cursor.getString(breedColumnIndex);
+        String petGender = cursor.getString(genderColumnIndex);
+        String petWeight = cursor.getString(weightColumnIndex);
+
 
         if (TextUtils.isEmpty(petBreed)) {
             petBreed = context.getString(R.string.unknown_breed);
         }
 
+        String gender = "Unknown Gender";
+
+        int check_gender = Integer.parseInt(petGender);
+
+        if(check_gender == PetContract.PetEntry.GENDER_MALE){
+            gender = "Male";
+        }else if (check_gender == PetContract.PetEntry.GENDER_FEMALE){
+            gender = "Female";
+        }
+
+        String summary = "Breed: " + petBreed
+                + "\nGender: " + gender
+                + "\nWeight: " + petWeight;
         nameTextView.setText(petName);
-        summaryTextView.setText(petBreed);
+        summaryTextView.setText(summary);
     }
 }
